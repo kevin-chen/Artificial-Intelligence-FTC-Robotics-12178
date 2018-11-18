2271af1ac68ee09f4ed14e1b72463bc7e524e6e5
Kevin Chen <kc3585@nyu.edu>
2018-11-18 16:35:23 -0500

    Update TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueCloserToRR.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueCloserToRR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueCloserToRR.java
@@ -11,9 +11,6 @@ import org.firstinspires.ftc.teamcode.Helper.Pictograph;
 import org.firstinspires.ftc.teamcode.Helper.Timer;
 import org.firstinspires.ftc.teamcode.Robot.AutoRobot;
 
-
-//This is a test for github
-//deleting on web github
 /**
  * Created by Artificial Intelligence 12178 on 1/16/2018.
  */
